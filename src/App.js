b4298656bd7885bea28e857323fd1485376498c2
Om <ompol1705@gmail.com>
2024-03-05 00:05:00 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
+  const user = localStorage.getItem("token");
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+          {user && <Route path="/" exact element={<Home />} />}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
+        </Routes>
+      </Router>
     </>
   );
 }
